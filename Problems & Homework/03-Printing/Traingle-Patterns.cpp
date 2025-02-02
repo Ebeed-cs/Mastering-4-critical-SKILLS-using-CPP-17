27ffa000c1729d242a08ed49fd027c12d55a8785
Ahmed Ebeed <aebeedcs@gmail.com>
2025-02-02 09:00:37 +0200

    ADD 01 in Printing

---
+++ Problems & Homework/03-Printing/Traingle-Patterns.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Pattern 1
+	cout << "*" << endl;
+	cout << "* *" << endl;
+	cout << "* * *" << endl;
+	cout << "* * * *" << endl;
+	cout << "* * * * *" << endl;
+
+	// Pattern 2
+	cout << "        *        " << endl;
+	cout << "      * * *      " << endl;
+	cout << "    * * * * *    " << endl;
+	cout << "  * * * * * * *  " << endl;
+	cout << "* * * * * * * * *" << endl;
+	// pattern 3
+	cout << "    *    " << endl;
+	cout << "  * * *  " << endl;
+	cout << "* * * * *" << endl;
+	cout << "  * * *  " << endl;
+	cout << "    *    " << endl;
+	return 0;
+}
