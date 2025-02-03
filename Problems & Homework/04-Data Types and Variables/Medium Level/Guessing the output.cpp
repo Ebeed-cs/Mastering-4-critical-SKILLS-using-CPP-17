9aa8df548759b45822d0948bae8505fca9330f7f
Ahmed Ebeed <aebeedcs@gmail.com>
2025-02-03 10:17:18 +0200

    ADD 04/medium/ 2problems

---
+++ Problems & Homework/04-Data Types and Variables/Medium Level/Guessing the output.cpp
@@ -0,0 +1,38 @@
+// Guess the Output
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3;
+
+	num1 = 0, num2 = 1, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+
+	// https://en.wikipedia.org/wiki/Fibonacci_number
+
+	return 0;
+}
+
+// ___________________________________
+
+/*
+	1
+	2
+	3
+	5
+	8
+	13
+	21
+	34
+	55
+
+*/
