8dc497d31b56d211e1e1d0d72a23f9e91a76b3b6
Ahmed Ebeed <aebeedcs@gmail.com>
2025-02-05 07:41:42 +0200

    UPD Guess the output3.cpp - operators

--- Problems & Homework/05-Operators/01-Easy to Medium/Guess the output3.cpp
+++ Problems & Homework/05-Operators/01-Easy to Medium/Guess the output3.cpp
@@ -2,12 +2,37 @@
 using namespace std;
 
 /*
-    Guess the output3
+	Guess the output3
 */
 
 int main() {
-    int i = 0;
-    cout << i << " " << ++i << endl; // 0 1
-    // Undefined Behavior until C++17
-    return 0;
+
+	int a = 210;
+
+	a /= 2;  		// 105
+	cout<<a<<"\n"; // 105 [output]
+
+	cout<<(a /= 3)<<"\n";  // 35	[output]
+	cout<<(a /= 5)<<"\n";	// 7	[output]
+	cout<<(a /= 7)<<"\n";	// 1	[output]
+
+	cout<<(2+3) * (5-(-3)) / 5 / 8 <<"\n"; // 1 [output]
+
+	a = 10;
+	cout<<a++ + 10<<"\n";  // 20 [output]
+	cout<<++a + 10<<"\n";  // 22 [output]
+	cout<<a-- + 10<<"\n"; //  22 [output]
+	cout<<--a + 10<<"\n"; //  20 [output]
+
+	int b = 20;
+	cout<<a++ + ++b<<"\n";	// 31 [output]
+
+	cout<<a<<"\n"; // 11 [output]
+	++a+=10;	   // 22 
+	cout<<a<<"\n";	// 22 [output]
+
+
+
+	return 0;
 }
+
