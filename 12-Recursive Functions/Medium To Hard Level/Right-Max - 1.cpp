f50a75c990e057f63eaef141e176bcb8da4f6c64
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:58:51 +0200

    Update Right-Max - 1.cpp

--- 12-Recursive Functions/Medium To Hard Level/Right-Max - 1.cpp
+++ 12-Recursive Functions/Medium To Hard Level/Right-Max - 1.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 using namespace std;
-/*
-        Right-Max - 1
-
-    ● Given array, change each element at position i to be the maximum of numbers from index i to end of array
-    ● E.g. input 1 3 5 7 4 2 ⇒ [7, 7, 7, 7, 4, 2]
-    ● Void right_max(int arr[], int len, int start_position = 0);
-
-*/
 
 void right_max(int arr[], int len, int start_position = 0) {
     if (start_position)
