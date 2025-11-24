08a24601694abf6da7c561ce22cbee39bf81397d
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:43:17 +0200

    Update Grouping.cpp

--- 09-Char Arrays/Easy to Medium Challenges/Grouping.cpp
+++ 09-Char Arrays/Easy to Medium Challenges/Grouping.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                        Grouping
-
-    ● Read a string, then divide it to consecutive groups of same letter. Print each group
-
-    ● Input ⇒ outputs.
-        ○ “111222aabbb” ⇒ 111 222 aa bbb
-        ○ HHHH ⇒ HHHH
-
-*/
 
 int main() {
     string str;
