c3ebdc0ca69cdc8f374d63f0a92ca12568088ab4
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:59:46 +0200

    Update Trace.cpp

--- 12-Recursive Functions/Medium To Hard Level/Trace.cpp
+++ 12-Recursive Functions/Medium To Hard Level/Trace.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-	   Trace
 
-	● Without running code
-		○ Trace by hand: What does this method do?
-		○ What happens if we swapped lines 6 & 7?
-
-		void do_something(int n) {
-			if (n) {
-			cout << n % 10;
-			do_something(n /10);
-}}
-
-			int main() {
-			do_something(123456);
-			return 0;
-
-		}
-
-*/
 
 void do_something1(int n) {	// print number digit by digit reversed -> // 654321
 	if (n) {
