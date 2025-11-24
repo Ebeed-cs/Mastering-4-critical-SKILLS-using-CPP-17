647fbf8724686c6a08631268cf595348d8bdbf68
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:07:45 +0200

    Update Swapping two variables.cpp

--- 04-Data Types and Variables/Medium Level/Swapping two variables.cpp
+++ 04-Data Types and Variables/Medium Level/Swapping two variables.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-Homework 6: Swapping 2 numbers!
 
-Write a program that reads 2 variables num1 and num2
-E.g. say we read num1 = 7 and num2 = 231
-
-Target: we want swap the values of Num1 and Num2?
-Swap means exchange
-
-So Num1 takes value 231 and Num2 takes value 7
-
-*/
 int main() {
    int num1 = 7 , num2 = 231;
    num2 = num2 + num1;
