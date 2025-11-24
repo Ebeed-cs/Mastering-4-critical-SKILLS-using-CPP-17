b22d4388b293089c4b8577d4865cb7a9069a1291
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:21:40 +0200

    Update Print Range.cpp

--- 07-Loops/Easy Level/Print Range.cpp
+++ 07-Loops/Easy Level/Print Range.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-			Print Range
-		● Given a starting number X and an ending number Y, print all numbers between X and Y inclusive, each on a line.
-	
-		● Input 3 7
-		● Output
-			○ 3
-			○ 4
-			○ 5
-			○ 6
-			○ 7
-
-
-*/
 
 int main() {
 	int x , y;
