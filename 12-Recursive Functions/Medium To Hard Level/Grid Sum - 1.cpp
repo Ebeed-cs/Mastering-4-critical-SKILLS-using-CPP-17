16a83207e3e09435768f54dd5eb8aca12ab6359e
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:58:14 +0200

    Update Grid Sum - 1.cpp

--- 12-Recursive Functions/Medium To Hard Level/Grid Sum - 1.cpp
+++ 12-Recursive Functions/Medium To Hard Level/Grid Sum - 1.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-	   Grid Sum - 1
 
-	● Given a 2D array of numbers, all of them are positive distinct. Robot start from (0, 0). It can move to the right or bottom or diagonal. See the arrows.
-	● It will select one direction: the maximum. Print the total path sum of this robot
-		○ int path_sum(int grid[100][100], int row, int col, int ROWS, int COLS)
-
-	● Input
-		○ 3 3
-		○ 1 7 8
-		○ 2 10 11
-		○ 20 5 9
-	● Output: 31 (from 1 + 10 + 11 + 9)
-		○ Robot start at (0, 0). 3 possible values (2, 7, 10). Max 10, so go to this cell
-		○ Then 3 possible values (5, 9, 11). Go to 11. Then only 9 available
-
-*/
 int di[] = { 0 , 1 ,1 };
 int dj[] = { 1 , 1, 0 };
 
