451cb99139aa96cffc8c988c940431e162d135cc
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 10:14:41 +0200

    Update Remove All Adjacent Duplicates In String.cpp

--- 17-STL/Medium Challenges/Remove All Adjacent Duplicates In String.cpp
+++ 17-STL/Medium Challenges/Remove All Adjacent Duplicates In String.cpp
@@ -2,34 +2,6 @@
 #include <stack>
 using namespace std;
 
-/*
-                    Remove All Adjacent Duplicates In String
-
-            https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
-                    
-            Given a string s of lowercase letters, a duplicate removal consists of choosing two
-            adjacent and equal letters, and removing them.
-
-            We repeatedly make duplicate removals on S until we no longer can.
-
-            Return the final string after all such duplicate removals have been made. It is
-            guaranteed the answer is unique.
-
-            Use a stack
-            · string
-            · removeDuplicates(string S)
-
-            Example 1:
-
-            Input: "abbaca"
-            Output: "ca"
-            Explanation:
-            For example, in "abbaca" we could remove "bb" since the letters
-            are adjacent and equal, and this is the only possible move. The
-            result of this move is that the string is "aaca", of which only
-            "aa" is possible, so the final string is "ca".
-
-*/
 
 string removeDuplicates(string S) {
     stack<char> st;
