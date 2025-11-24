ee5abb5c8c9aa2be387e9d9919155fc89e1a1106
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:11:24 +0200

    Update Create logic!.cpp

--- 05-Operators/02-Easy to Medium/Create logic!.cpp
+++ 05-Operators/02-Easy to Medium/Create logic!.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	Create logic!
-● Write a program that reads 3 integers about the class room
-○ Number of boys (nb), number of girls (ng), number of teachers (nt)
-● Prepare and print a boolean variable for 
-these cases:
-	● nb greater than 25
-	● ng less than or equal to 30
-	● nb > 20 and nt > 2 or ng > 30 and nt > 4
-	● Either nb < 60 or ng < 70
-	● Neither nb >= 60 nor ng >= 70
-	● nb is 10 more students than ng
-	● Difference between nb and ng is more than 10 or nt > 5
-	● Either nb is 10 more students than ng or ng is 15 more students than nb
-*/
 
 int main() {
 	int nb, ng, nt;
