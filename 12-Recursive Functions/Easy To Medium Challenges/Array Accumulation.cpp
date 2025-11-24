a04a53f4593e53d1b718a548e3e3dde084dd7958
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:56:57 +0200

    Update Array Accumulation.cpp

--- 12-Recursive Functions/Easy To Medium Challenges/Array Accumulation.cpp
+++ 12-Recursive Functions/Easy To Medium Challenges/Array Accumulation.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-        Array Accumulation
 
-    ● Given an array we want to accumulate it as following:
-        ○ Input 1 2 3 4 5 6
-        ○ Output array
-            ■ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5, 1+2+3+4+5+6
-            ■ 1, 3, 6, 10, 15, 21
-        ○ That is return arr[i] = sum of all numbers from 0 to i
-    ● void accumulate_arr(int arr[], int len);
-        ○ Input 1 8 2 10 3 ⇒ 1 9 11 21 24
-
-*/
 void accumulate_arr(int arr[], int len) {
     if (len == 1) return;
     accumulate_arr(arr , len - 1);
