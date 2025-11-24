c61a4c65248e42a140094a3cbc82047ecd3bce47
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:59:53 +0200

    Update Print 3n+1 Sequence.cpp

--- 12-Recursive Functions/Practice lecture/Print 3n+1 Sequence.cpp
+++ 12-Recursive Functions/Practice lecture/Print 3n+1 Sequence.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-        Print 3n+1 Sequence
 
-    ● A 3n+1 goes as following
-    ● Start from a number n
-    ● If this number is even, next number in sequence is n / 2
-    ● If this number is odd, next number in sequence is 3 * n + 1
-    ● If this number is 1 = end of sequence
-    ● E.g. Start from 5: 5 16 8 4 2 1
-    ● E.g. Start from 6: 6 3 10 5 16 8 4 2 1
-    ● E.g. Start from 7: 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
-    ● Write a recursive function to print it
-
-*/
 
 void sequence(int n)
 {
