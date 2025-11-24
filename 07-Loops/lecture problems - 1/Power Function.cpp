9c1fe17bbdc51e1fcd465841eb2693235d308941
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:26:27 +0200

    Update Power Function.cpp

--- 07-Loops/lecture problems - 1/Power Function.cpp
+++ 07-Loops/lecture problems - 1/Power Function.cpp
@@ -1,13 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	 Read 2 integers x and Y and compute XY.
-		○ This means X * X * X ….. Y times
-		○ E.g = 2 5 = 2 * 2 * 2 * 2 * 2
-
-
-*/
 
 int main() {
 	int num, pow, result = 1;
