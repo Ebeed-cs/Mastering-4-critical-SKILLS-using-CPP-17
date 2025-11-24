fed3207dfd31d53d0c10939bc1c1a0216bf4e860
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:24:48 +0200

    Update Special multiples 2.cpp

--- 07-Loops/Medium Level/Special multiples 2.cpp
+++ 07-Loops/Medium Level/Special multiples 2.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-		Special multiples 2
-
-	● Read an integer N : Print the first N numbers that are
-		○ multiple of 3 but not multiple of 4
-
-	● Input: 11
-	● Output: 3 6 9 15 18 21 27 30 33 39 42
-
-	● Notice
-		 ○ 12 is divisible by both 3 and 4 ⇒ so excluded
-
-*/
-
 int main() {
 	int N, count = 0;
 	int i = 0;
