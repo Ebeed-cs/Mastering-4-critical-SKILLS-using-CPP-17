8686c9b80eaff2d4e6b946ff8d6abe3cadaeea69
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:17:00 +0200

    Update Find Maximum up to 10.cpp

--- 06-Selection/Medium Level/Find Maximum up to 10.cpp
+++ 06-Selection/Medium Level/Find Maximum up to 10.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-		   Find Maximum up to 10
- ● Read an integer N (2 <= N <= 10)
- ● Then read N integers, find which of them has the biggest value and print it.
-
- ● Inputs
-	○ 5  1 3 2 4 2 ⇒ 4
-		■ 5 means read 5 integers
-		■ Then we read them [1 3 2 4 2]. Their maximum is 4
-
-	○ 10 1  67  -9  88  -45  129  90  65  77  34 ⇒ 129
-		■ Same as last homework. This time we are given first N (10)
-
-
-*/
 
 
 int main() {
