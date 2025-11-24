15b1236ceff59bb5949eeec5b9eddee9b7297c39
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:30:03 +0200

    Update Is Palidrome.cpp

--- 08-1D Arrays/Easy to Medium Level/Is Palidrome.cpp
+++ 08-1D Arrays/Easy to Medium Level/Is Palidrome.cpp
@@ -1,25 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-				Is Palindrome?
-
-	● Read integer N (< 1000), then read N integers of an array.
-	● Determine if the array is palindrome or not.
-	● An array is called palindrome if it reads the same backward and forward
-		○ for example, arrays { 1 } and { 1,2,3,2,1 } are palindrome
-		○ while arrays { 1,12 } and { 4,7,5,4 } are not.
-
-	● Inputs ⇒ Outputs
-		○ 5   1 3 2 3 1  ⇒ YES
-		○ 4   1 2 3 4  ⇒ NO
-
-
-
-
-
-*/
 
 int main() {
 	int N;
