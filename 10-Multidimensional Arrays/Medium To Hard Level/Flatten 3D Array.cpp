6ff0626cca4d53e1d6429a19299e0ea9c8804739
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:49:06 +0200

    Update Flatten 3D Array.cpp

--- 10-Multidimensional Arrays/Medium To Hard Level/Flatten 3D Array.cpp
+++ 10-Multidimensional Arrays/Medium To Hard Level/Flatten 3D Array.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-               Flatten 3D Array
 
-    ● Then read integer either 1 (convert 3D to 1D) or 2 (1D to 3D)
-    ● If input was 1, then read 3 integers d, r, c then convert to position in 1D array
-    ● If input was 2, then read 1 integer position, then convert to 3D array position
-    ● Try to generalize if we have e.g. 6D array
-
-    ● Input ⇒ Outputs
-        ○ 3 4 5 1 1 0 0 ⇒ 20
-        ○ 3 4 5 2 20 ⇒ 1 0 0
-        ○ 3 4 5 1 1 1 1 ⇒ 26
-        ○ 3 4 5 1 2 3 2 ⇒ 57
-        ○ 3 4 5 1 2 0 0 ⇒ 40
-        ○ 3 4 5 2 59 ⇒ 2 3 4
-
-*/
 
 int dep, row, col;
     cin >> dep >> row >> col;
