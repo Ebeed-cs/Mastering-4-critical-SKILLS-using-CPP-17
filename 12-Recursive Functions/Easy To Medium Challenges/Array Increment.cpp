7c53912d60a136ee169bfb47a82b34781c4fde71
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:57:05 +0200

    Update Array Increment.cpp

--- 12-Recursive Functions/Easy To Medium Challenges/Array Increment.cpp
+++ 12-Recursive Functions/Easy To Medium Challenges/Array Increment.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 using namespace std;
-/*
-        Array Increment
-
-    ● void array_increment(int arr[], int len)
-    ● The function increments each arr[i] with i
-    ● E.g. for input
-        ○ [1, 2, 5, 9] it be [1+0, 2+1, 5+2, 9+3]
-        ○ 1 8 2 10 3 ⇒ 1 9 4 13 7
-
-*/
 
 void array_increment(int arr[], int len) {
     if (len == 0) return;
