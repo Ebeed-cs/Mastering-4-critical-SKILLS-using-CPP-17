1cdc765fea942de8dbd0daed5926432c0616cdb2
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:47:15 +0200

    Update Active Robot - 1.cpp

--- 10-Multidimensional Arrays/Medium To Hard Level/Active Robot - 1.cpp
+++ 10-Multidimensional Arrays/Medium To Hard Level/Active Robot - 1.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-                Active Robot - 1
 
-    ● Read integers N, M represents a matrix. A robot start at cell (0, 0).
-    ● Read integer K, then K commands. Each command is 2 values
-        ○ Direction from 1 to 4: up, right, down, left
-        ○ Steps: a number to number steps to take in the direction. Steps [1, 1000000000]
-        ○ If the robot hits the wall during the move, it circulates in the matrix.
-        ○ For every command, print where is the robot now
-
-    ● Input
-        ○ 3 4  4   2 1   3 2   4 2   1 3
-            ■ 2 1 means to right 1 step - 3 2 means down 2 steps
-    ● Output
-        ○ (0, 1) (2,1) (2, 3) (2, 3)
-
-*/
 
 int main() {
     int row , col;
