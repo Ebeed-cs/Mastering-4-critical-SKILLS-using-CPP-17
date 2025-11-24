051cf23fbefde9a0b6bec69f039a07366540938e
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:26:20 +0200

    Update Numbers divisible by 3.cpp

--- 07-Loops/lecture problems - 1/Numbers divisible by 3.cpp
+++ 07-Loops/lecture problems - 1/Numbers divisible by 3.cpp
@@ -1,11 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	Read an integer X, find all numbers divisible by 3 from 1 to X.
-		○ These are 3, 6, 9, 12, 15, 18, ….. (multiple of 3)
-
-*/
 
 int main() {
 	int x, result = 1;
