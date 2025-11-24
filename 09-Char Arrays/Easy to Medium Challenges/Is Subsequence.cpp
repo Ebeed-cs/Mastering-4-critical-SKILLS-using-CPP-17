f2994b896a1ecd2cce2613c7129bbbf1e9f84ce2
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:43:32 +0200

    Update Is Subsequence.cpp

--- 09-Char Arrays/Easy to Medium Challenges/Is Subsequence.cpp
+++ 09-Char Arrays/Easy to Medium Challenges/Is Subsequence.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                        Is Subsequence?
-
-    ● Read 2 strings input and str. Print YES if the given str is a subsequence for
-        the string. Otherwise, print NO
-
-    ● Input ⇒ Outputs
-        ○ ABCDEFG ABCD ⇒ YES
-        ○ ABCDEFG ABCDEF ⇒ YES
-        ○ ABCDEFG EFG ⇒ YES
-        ○ ABCDEFG DEFG ⇒ YES
-        ○ ABCDEFG BCD ⇒ YES
-        ○ ABCDEFG DEF ⇒ YES
-        ○ ABCDEFG ACEG ⇒ YES
-        ○ ABCDEFG DG ⇒ YES
-        ○ ABCDEFG GD ⇒ NO
-        ○ ABCDEFG ABCDEFG ⇒ YES
-
-*/
-
 int main() {
     string input, str;
     cin >> input >> str;
