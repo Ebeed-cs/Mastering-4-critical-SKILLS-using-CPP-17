e5485660092f3dae0a9bdc99664dc929a77b2ed6
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:30:13 +0200

    Update Is increasing array.cpp

--- 08-1D Arrays/Easy to Medium Level/Is increasing array.cpp
+++ 08-1D Arrays/Easy to Medium Level/Is increasing array.cpp
@@ -1,21 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-				Is increasing array?
-
-	● Read an Integer N, then read N (<= 200) integers. 
-	● Print YES if the array is increasing. 
-		○ An array is increasing if every element is >= the previous number
-
-	● Inputs  
-	○ 4     1 2 2 5   ⇒ YES
-	○ 5     1 0 7 8 9  ⇒ NO   [0 is < 1, the previous number]
-	○ 2     -10 10 ⇒ YES 
-	
-	
-*/
-
 int main() {
 	int N;
 	cin >> N;
