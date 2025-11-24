1822c33fc8dc5790c3f0f4c17e572b9d312e22a6
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:47:10 +0200

    Update Triangular matrix - 2.cpp

--- 10-Multidimensional Arrays/Easy Level/Triangular matrix - 2.cpp
+++ 10-Multidimensional Arrays/Easy Level/Triangular matrix - 2.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                Triangular matrix - 2
-
-    ● Read integer N, then Read Square matrix NxN. Then, print 2 values. The sum of the lower triangle matrix and the upper triangle.
-
-    ● Input
-        ○ 3
-        ○ 8 16 9
-        ○ 3 15 27
-        ○ 14 25 29
-    ● Output
-        ○ 94 (8+15+29+3+25+14)
-        ○ 104 (8+15+29+16+27+9)
-
-*/
 
 int main() {
 	int n, upper = 0, lower = 0, val;
