842a4e21d9fa87d77ceee293a502c7a80295d003
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 10:17:25 +0200

    Update Sum of last K numbers (stream) - 2.cpp

--- 17-STL/STL Practice 1/Sum of last K numbers (stream) - 2.cpp
+++ 17-STL/STL Practice 1/Sum of last K numbers (stream) - 2.cpp
@@ -2,18 +2,6 @@
 #include <queue>
 using namespace std;
 
-/*
-                    Sum of last K numbers (stream) - 2
-
-            ● This struct will receive stream of numbers, each time return sum of last k numbers
-            ● E.g. if k = 4
-            ● Stream: 1 2 3 4 5 6 7 8 9
-            ● Returns: 1, 1+2, 1+2+3,1+2+3+4, 2+3+4+5, 3+4+5+6, ..
-                ○ That is for 6 ⇒ 18
-            ● Don’t store more than ~ K numbers
-
-*/
-
 struct last_k_numbers_sum_stream {
 	int k;
 
