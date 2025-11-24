0d48bf2b2c4555544d69999a51f2460244c02e33
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:16:11 +0200

    Update Sort 3 numbers.cpp

--- 06-Selection/Easy Level/Sort 3 numbers.cpp
+++ 06-Selection/Easy Level/Sort 3 numbers.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 
 						// [Explanation video]
-/*
-	 Sort 3 numbers
- ● Given 3 integers, sort (order) them in ascending order and print them .
-
- ● Inputs ⇒ outputs
- ○ 1 2 3 ⇒ 1 2 3
- ○ 1 3 2 ⇒ 1 2 3
- ○ 2 1 3 ⇒ 1 2 3
- ○ 2 3 1 ⇒ 1 2 3
- ○ 3 1 2 ⇒ 1 2 3
- ○ 3 2 1 ⇒ 1 2 3
- ● Do you notice there are only 6 ways to permutate 3 numbers!
-
-
-*/
 
 
 int main() {
