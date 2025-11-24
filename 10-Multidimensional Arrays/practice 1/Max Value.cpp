7459bca66c009ec94f2664aa021ac0c762aabd63
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:50:05 +0200

    Update Max Value.cpp

--- 10-Multidimensional Arrays/practice 1/Max Value.cpp
+++ 10-Multidimensional Arrays/practice 1/Max Value.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-            Max value
-
-        ● Read 2 integers for the rows and columns of a matrix ( <= 100). Then read rows x cols integer value. Find the position of maximum value in the array. If there are several ones, find the last occurance
-
-        ● Input:
-            ○ 3 4
-            ○ 1 5 1 10
-            ○ 2 10 3 4
-            ○ 1 10 10 7
-        ● Output
-            ○ Max value at position 2 2 with value = 10
-*/
 
 int main() {
     int row, col;
