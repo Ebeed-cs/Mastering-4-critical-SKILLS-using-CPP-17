4caa622af7c787b27e93ee32e9ac94e32108a151
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:39:00 +0200

    Update Sorting numbers.cpp

--- 08-1D Arrays/Medium Level/Sorting numbers.cpp
+++ 08-1D Arrays/Medium Level/Sorting numbers.cpp
@@ -1,19 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-			Sorting numbers
-
-	● Read integer N ( <= 900), followed by reading N integers (0 <= value <= 500)
-	● Print the sorted list of the numbers
-
-	● Input:    13      1 5 5 2 5 7 2 3 3 3 5 2 7
-	● Output: 1 2 2 2 3 3 3 5 5 5 5 7 7
-	● Give your most efficient trial
-		○ You don’t need to google how to sort numbers
-		○ Hint: max value in the array is 500
-
-*/
 
 int main() {
 	int N;
