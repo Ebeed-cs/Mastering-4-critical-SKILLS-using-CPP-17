116dd3937bcf4705be48834ecfd0c7724867a4b7
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:16:53 +0200

    Update Find Maximum of 10.cpp

--- 06-Selection/Medium Level/Find Maximum of 10.cpp
+++ 06-Selection/Medium Level/Find Maximum of 10.cpp
@@ -1,18 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-		  Find Maximum of 10
- ● Read 10 integers, find which of them has the biggest value and print it.
- ● Inputs
- ○ 1  67  -9  88  -45  129  90  65  77  34 ⇒ 129
- ● Restriction: In your whole code there should be 2 integer variables defined 
-	ONLY
- ○ If it is hard constraint; code it in whatever way
-
-
-*/
-
 
 int main() {
 	int num, max_num;
