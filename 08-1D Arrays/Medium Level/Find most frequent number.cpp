c5ced6637d6c748ff8a4c54602fff2e4019a2bc0
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:38:25 +0200

    Update Find most frequent number.cpp

--- 08-1D Arrays/Medium Level/Find most frequent number.cpp
+++ 08-1D Arrays/Medium Level/Find most frequent number.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-		Find most frequent number
-
- 	● Read an Integer N, then read N <= 200 integers. Find the value that repeated the most number of times.
- 		○ Each integer is -500 <= value <= 270
- 	● Example for array: 7    -1 2 -1 3 -1 5 5
- 	○ -1 repeated 3 times: the largest
- 	● Don’t use nested loops
-
-
-*/
 
 int main() {
 	int N;
