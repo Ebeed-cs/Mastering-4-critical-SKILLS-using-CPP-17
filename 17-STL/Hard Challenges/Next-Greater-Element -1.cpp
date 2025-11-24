d47d0f75e62d9bcd982a0b74ae7d3b5d257e26b8
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 10:13:42 +0200

    Update Next-Greater-Element -1.cpp

--- 17-STL/Hard Challenges/Next-Greater-Element -1.cpp
+++ 17-STL/Hard Challenges/Next-Greater-Element -1.cpp
@@ -2,24 +2,6 @@
 #include <vector>
 using namespace std;
 
-/*
-                           Next Greater Element - 1
-
-                           https://leetcode.com/problems/daily-temperatures/description/
-
-        ● Given an array, for every number, find the first number after it of a higher value
-        ● E.g. 10, 5, 7, 15, 11 ⇒ 15 7 15 -1 -1
-            ○ First number greater than 10 ⇒ 15
-            ○ First number greater than 5 ⇒ 7
-            ○ First number greater than 7 ⇒ 15
-            ○ First number greater than 15 and 11 ⇒ None, so use -1
-        ● Input:
-            ○ 8 ,73, 74, 75, 71, 69, 72, 76, 73 ⇒ 73 74 75 76 72 72 76 -1 -1
-        ● We can easily solve that with 2 nested loops
-        ● We can solve it with 1 loop using stack. Find it
-
-
-*/
 
 vector<int> next_greater(vector<int>& v) {
     vector<int> result;
