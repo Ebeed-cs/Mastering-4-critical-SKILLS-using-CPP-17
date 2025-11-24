4340c22c3a05bf827805e33c3da58d05fa4ebb2a
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:12:00 +0200

    Update Our remainder.cpp

--- 05-Operators/03-Easy Level/Our remainder.cpp
+++ 05-Operators/03-Easy Level/Our remainder.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	 Our remainder
- ● We know N % M computes the remainder of division
- ● Write a program that reads 2 positive integers and print such reminder without using the modulus operator %
- ● Input: 27 12
- ● Output: 3
- ○ Remember in math: 27 % 12 = 3
-
-
-
-*/
 int main() {
 	int num1, num2;
 	cin >> num1 >> num2;
