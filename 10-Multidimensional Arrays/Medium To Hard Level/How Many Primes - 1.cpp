0fd29008306037822602b69f7feaff64de7b2a89
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:49:14 +0200

    Update How Many Primes - 1.cpp

--- 10-Multidimensional Arrays/Medium To Hard Level/How Many Primes - 1.cpp
+++ 10-Multidimensional Arrays/Medium To Hard Level/How Many Primes - 1.cpp
@@ -2,23 +2,6 @@
 using namespace std;
 /*
             [Explanation video]
-                How many primes - 1
-
-    ● Read 2 integers N and M, then Read matrix NxM.
-    ● Then read integer Q, for Q queries.
-    ● For each query read 4 integers: i j r c
-        ○ Represents a grid (submatrix): top left (i, j) and (r, c) for (# rows, # cols)
-    ● For each query, print how many prime numbers in the requested grid.
-        ○ Each query should be answered using nested loops maximum not more
-
-    Input ⇒ Output
-    ○ 3 4
-    ○ 8 2 9 5
-    ○ 3 2 27 6
-    ○ 7 8 29 22
-    ○ 2
-    ○ 1 0 2 2 ⇒ 3 (primes 3, 2, 7 in rectangle (0, 1) (2, 1) )
-    ○ 0 1 2 3 ⇒ 3 (primes 2, 5, 2 in rectangle (0, 1) (1, 3) )
 */
 
  // Function to check if a number is prime
