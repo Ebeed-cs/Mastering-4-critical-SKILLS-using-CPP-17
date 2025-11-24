591e7b34817b3bb8eaca12425161f84b436e22ee
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:31:00 +0200

    Update Unique Numbers of ordered list.cpp

--- 08-1D Arrays/Easy to Medium Level/Unique Numbers of ordered list.cpp
+++ 08-1D Arrays/Easy to Medium Level/Unique Numbers of ordered list.cpp
@@ -1,25 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-				Unique Numbers of ordered list 
-
-	● Read integer N ( < 1000), followed by reading N integers (0 <= value <= 500)
-	● The N numbers are ordered from small to large
- 	● Print the unique list of the numbers, but preserve the given order
- 
-	● Input:    12       1 1 2 2 2 5 6 6 7 8 9 9
-	● Output: 1 2 5 6 7 8 9
- 		○ Observe: input is sorted list
- 	● Optional Constraints:
- 		○ Don’t use nested loops! 
-		○ Use only 1 single array
-		○ Or Do it without even using arrays at all
-
-
-*/
-
 int main() {
 	int N , prev , next;
 	prev = -1;
