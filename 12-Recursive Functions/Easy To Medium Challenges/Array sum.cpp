4ddc286052bcb2e4f6fb3a9a1accfe53cd7ae5be
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:57:34 +0200

    Update Array sum.cpp

--- 12-Recursive Functions/Easy To Medium Challenges/Array sum.cpp
+++ 12-Recursive Functions/Easy To Medium Challenges/Array sum.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-        Array sum
 
-    ● Int sum(int arr[], int len);
-    ● Write a function that computes array sum
-    ● Input 1, 8, 2, 10, 3 ⇒ 24
-
-*/
 
 int arr_sum(int arr[], int len) {
     if (len == 1) return arr[0];
