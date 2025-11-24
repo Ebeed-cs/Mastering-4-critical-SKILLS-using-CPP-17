b925e3f3d42a552caa99b6a7a8f404134a4ece75
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:06:54 +0200

    Update Swapping 3 numbers!.cpp

--- 04-Data Types and Variables/Hard Level/Swapping 3 numbers!.cpp
+++ 04-Data Types and Variables/Hard Level/Swapping 3 numbers!.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-Swapping 3 numbers!
 
-Write a code to swap 3 numbers
-Let say we have numbers a = 115, b = 20, c = 301
-We wanna their final values to be: a = 20, b = 301, c = 115
-
-*/
     // MY SOLUTION USING ONLY THREE VARIABLES 
 int main() {
    int a = 115, b = 20 , c = 301;
