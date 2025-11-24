74c5eb0904a6c23cf351e5283c4b0b7ebc32d6c5
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:43:26 +0200

    Update Is Prefix.cpp

--- 09-Char Arrays/Easy to Medium Challenges/Is Prefix.cpp
+++ 09-Char Arrays/Easy to Medium Challenges/Is Prefix.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                        Is Prefix?
-
-        ● Read 2 strings input and str. Print YES if the given str is a prefix for the string.Otherwise, print NO
-
-        ● Input ⇒ Outputs
-            ○ ABCDEFG ABCD ⇒ YES
-            ○ ABCDEFG ABCDEF ⇒ YES
-            ○ ABCDEFG EFG ⇒ NO
-            ○ ABCDEFG DEFG ⇒ NO
-            ○ ABCDEFG BCD ⇒ NO
-            ○ ABCDEFG DEF ⇒ NO
-            ○ ABCDEFG ACEG ⇒ NO
-            ○ ABCDEFG DG ⇒ NO
-            ○ ABCDEFG GD ⇒ NO
-            ○ ABCDEFG ABCDEFG ⇒ YES
-
-*/
 
 int main() {
     string input , str;
