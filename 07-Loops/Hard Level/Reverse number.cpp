c8110028ef3da2c60b8d7c5dde78e81f153bcec5
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:22:10 +0200

    Update Reverse number.cpp

--- 07-Loops/Hard Level/Reverse number.cpp
+++ 07-Loops/Hard Level/Reverse number.cpp
@@ -1,16 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-							Reverse number
-	● Read an integer N, then find its reverse integer R
-		○ Print R R*3
-	● input ⇒ Output
-		○ 123  ⇒ 321 963
-
-
-
-*/
 
 int main() {
 	int N , result = 0;
