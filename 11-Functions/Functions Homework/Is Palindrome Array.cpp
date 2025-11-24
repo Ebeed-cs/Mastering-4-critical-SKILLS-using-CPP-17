8283ebbce389dbbf4b626844569f564c6d209e1a
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:52:18 +0200

    Update Is Palindrome Array.cpp

--- 11-Functions/Functions Homework/Is Palindrome Array.cpp
+++ 11-Functions/Functions Homework/Is Palindrome Array.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-            Is Palindrome Array
-    ● Read N, then N integers for an Array. Call a function with the array to check if the array is palindrome or note
-        ○ We already coded it before
-        ○ Just copy code and rearrange to call function with array
 
-*/
 void is_palidrome(int N) {
     int number[100];
     for (int i = 0; i < N / 2; ++i)
