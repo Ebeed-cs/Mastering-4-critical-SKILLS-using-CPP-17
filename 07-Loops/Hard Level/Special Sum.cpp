71a76b9726938ece0bd3ca1240fba6a254ad0d57
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:22:47 +0200

    Update Special Sum.cpp

--- 07-Loops/Hard Level/Special Sum.cpp
+++ 07-Loops/Hard Level/Special Sum.cpp
@@ -1,26 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-			Special Sum
-
-	● Read integer T for number of test cases.
-	● For each test case read integer N. Then read N integers a, b, c
-	● Output: Compute the sum of the following expression:
-		○ a, b*b, c*c*c, d*d*d*d, e*e*e*e*e……
-			■ That is the k-th number is repeated k times
-
-	● Input:
-		○ 2
-		○ 3   5 7 2
-		○ 4  1 2 3 4
-	● Output
-		○ 62                                         [as (5 + 7*7 + 2*2*2)  = 62]
-		○ 288                                       [as (1+2*2+3*3*3+4*4*4*4) = 288]
-
-
-
-*/
 
 int main() {
 	int T;
@@ -48,16 +28,3 @@ int main() {
 	}
 	return 0;
 }
-
-/*
-	the first  -> print one time
-	the second -> print two times
-	the third  -> print three times
-	the fourth -> print fourth times
-			and so on
-
-	We need 3 nested loops
-		loop over test cases
-		loop over reading numbers
-		loop to repeat the number K times (multiplication)
-*/
