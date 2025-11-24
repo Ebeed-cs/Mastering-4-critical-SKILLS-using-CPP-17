2deb8429a7774d3bd5d415867b35a02f61b30968
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:11:53 +0200

    Update Fractional2.cpp

--- 05-Operators/03-Easy Level/Fractional2.cpp
+++ 05-Operators/03-Easy Level/Fractional2.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 
 
-/*
-By https://www.udemy.com/user/anishdalal4/
-    Anish Dalal
-Alternative Solution for Division and Modulus (only print fractional part)
-
-* The fractional part is calculated by using the remainder. So for example 27/5 = (25 + 2) / 5 = 25/5 + 2/5. 
-* We are only interested in the 2/5 part. So we can calculate the remainder using modulo and then divide it by the second number.
-*/
 int main() {
 
     int a, b; 
