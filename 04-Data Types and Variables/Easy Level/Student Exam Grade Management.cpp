d8add7857f6c0e79d1cff38582d39c4ab03497aa
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:05:40 +0200

    Update Student Exam Grade Management.cpp

--- 04-Data Types and Variables/Easy Level/Student Exam Grade Management.cpp
+++ 04-Data Types and Variables/Easy Level/Student Exam Grade Management.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
 
-A teacher want a program that reads 2 students information about math exam
-	Read per student: name, id and grade
-	Then print them. See the picture in the slides
-
-
-Be a good software engineer
-	Think deeply in your selected data types
-	The teacher gives us this dialogue to guide us
-	Be careful from your assumptions?
-	Is exam’s grade an integer?
-
-
-*/
 int main() {
     string st_name1,st_name2 ;
     int st_id1,st_id2;
