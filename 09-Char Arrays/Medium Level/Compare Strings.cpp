8c568187f3563f5587aafddcae0eb1e456526dac
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:44:05 +0200

    Update Compare Strings.cpp

--- 09-Char Arrays/Medium Level/Compare Strings.cpp
+++ 09-Char Arrays/Medium Level/Compare Strings.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                       Compare strings
-
-    ● Read 2 strings, then output YES if the first smaller than or equal to second.
-                            Otherwise, output NO
-
-        ○ Don’t use < operator to compare strings. Use loops
-
-    ● Input ⇒ Output
-        ○ aaa aaa ⇒ YES
-        ○ aaaaa aa ⇒ NO
-        ○ abc d ⇒ YES
-        ○ dddddddddddddd xyz ⇒ YES
-        ○ azzzzzzzz za ⇒ YES
-        ○ za azzzzzzzz ⇒ NO
-
-*/
 
 int main() {
     string str1 , str2;
