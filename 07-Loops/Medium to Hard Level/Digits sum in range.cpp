05cf02bfa2d26503e9ce9da04a802cb51ffc9a50
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:25:04 +0200

    Update Digits sum in range.cpp

--- 07-Loops/Medium to Hard Level/Digits sum in range.cpp
+++ 07-Loops/Medium to Hard Level/Digits sum in range.cpp
@@ -2,20 +2,6 @@
 #include <cmath>
 using namespace std;
 
-/*
-
-					Digits sum in range
-
-	● Read three numbers N, A, B. Print the summation of the numbers between 1 and N whose sum of digits is between A and B(inclusilve).
-	● Input / Output
-		○ 20 2 5 ⇒ 84
-			■ Numbers whose sums of digits are between 2 and 5, are: 2,3,4,5,11,12,13,14, 20.
-	● E.g. digits sum of 13 is 4 : which is between (2, 5)
-		○ 10 1 2 ⇒ 13
-		○ 100 4 16 ⇒ 4554
-
-
-*/
 
 int main() {
 	int N, A, B, result = 0;
