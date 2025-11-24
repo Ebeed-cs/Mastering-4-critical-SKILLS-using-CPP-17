dce9b3d57171df97ad323c9b67aab3e0d8b92c97
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:57:44 +0200

    Update left_max.cpp

--- 12-Recursive Functions/Easy To Medium Challenges/left_max.cpp
+++ 12-Recursive Functions/Easy To Medium Challenges/left_max.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-        Left-Max
 
-    ● Given array, change each element at position i to be the maximum of numbers from 0 to index i
-    ● E.g. input 1 3 5 7 4 2 ⇒ [1, 3, 5, 7, 7, 7]
-    ● Void left_max(int arr[], int len);
-
-*/
 void left_max(int arr[], int len) {
     if (len == 1) return;
     left_max(arr, len - 1);
