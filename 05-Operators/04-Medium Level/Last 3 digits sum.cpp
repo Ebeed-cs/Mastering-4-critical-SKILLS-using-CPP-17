a83ae3b52a1b8107976f59b23989ed96fa10471e
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:12:44 +0200

    Update Last 3 digits sum.cpp

--- 05-Operators/04-Medium Level/Last 3 digits sum.cpp
+++ 05-Operators/04-Medium Level/Last 3 digits sum.cpp
@@ -1,19 +1,7 @@
 #include<iostream>
 using namespace std;
 
-/*
 
-	 Last 3 digits sum
- ● Write a program that reads an integer and prints the sum of its last 3 digits.
- ● Inputs ⇒ Outputs examples
-	○ 15    ⇒ 6
-	○ 125   ⇒ 8
-	○ 1000  ⇒ 0
-	○ 1001  ⇒ 1
-	○ 1234  ⇒ 9
-	○ 99999 ⇒ 27
-
-*/
 
 int main() {
 	int num, sum = 0;
