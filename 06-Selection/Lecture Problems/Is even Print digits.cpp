a82f26ffdd1ac05810e69970782daa6b14d936a9
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:16:28 +0200

    Update Is even Print digits.cpp

--- 06-Selection/Lecture Problems/Is even Print digits.cpp
+++ 06-Selection/Lecture Problems/Is even Print digits.cpp
@@ -1,20 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	Is even? Print digits
-	Read an integer N, then do the following
-		if the number is even: print last digit of it
-		if the number is odd: do following:
-			- if number < 1000, print last 2 digits
-			- if number > 1000 and number < 1000000, print last 4 digits
-			- otherwise, print its negative value
-	testing examples of good coverage: 
-		- 234 => even => 4
-		- 157 => 57
-		- 56169 => 6169
-		- 1000001 => -1000001
-*/
 
 int main() {
 	int num;
