d5b43dc723babcc6bc5eda3560ad56ca6bc59cd3
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:50:08 +0200

    Update Special Print.cpp

--- 10-Multidimensional Arrays/practice 1/Special Print.cpp
+++ 10-Multidimensional Arrays/practice 1/Special Print.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-            Special print
-
-        ● Read 2 integers for the rows and columns of a matrix ( <= 100). Then read rows x cols integer value.
-        ● Print the following 4 values
-            ○ The sum of the left diagonal & The sum of the right diagonal
-            ○ The sum of the last row & The sum of the last column
-
-        ● Input: 3 4
-            ○ 8 16 9 52
-            ○ 3 15 27 6
-            ○ 14 25 2 10
-        ● Output
-            ○ 25 104
-            ○ 51 68
-*/
-
 int main() {
     int row, col;
     cin >> row >> col;
