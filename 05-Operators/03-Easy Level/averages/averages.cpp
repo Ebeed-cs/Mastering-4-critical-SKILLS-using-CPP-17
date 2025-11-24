1456f28f23bf2670d50e03c2dac40e769b5a3302
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:12:17 +0200

    Update averages.cpp

--- 05-Operators/03-Easy Level/averages/averages.cpp
+++ 05-Operators/03-Easy Level/averages/averages.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 using namespace std;
 
-/*
-	 Averages
- ● Write a program that reads 5 numbers and print the following:
-	○ A) Their average
-	○ B) The sum of the first 3 numbers divided by the sum of the last 2 numbers
-	○ C) The average of the first 3 numbers divided by the average of the last 2 numbers.
- ○ What is the math relation between B and C?
 
-EX:
- ● Input 1 2 3 4  5
- ○ 3
- ○ 0.666666667
- ○ 0.444444444
-
-*/
 int main() {
 	double num1, num2, num3, num4, num5;
 	cin >> num1 >> num2 >> num3 >> num4 >> num5;
