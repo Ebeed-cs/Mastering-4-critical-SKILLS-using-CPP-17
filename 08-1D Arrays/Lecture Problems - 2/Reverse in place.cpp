caa70362591c5b06c3490bb9056196852a83df83
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:38:08 +0200

    Update Reverse in place.cpp

--- 08-1D Arrays/Lecture Problems - 2/Reverse in place.cpp
+++ 08-1D Arrays/Lecture Problems - 2/Reverse in place.cpp
@@ -1,24 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-					Reverse in place
-
-		● Read an Integer N, then read N <= 200 integers.
-			○ In-place: Change the current array, don’t use 2 arrays
-		● Simple idea: Iterate from the begin and end in same time
-			○ Swap the 2 positions
-			○ Do this tell the middle only
-		● Let say array is 1 2 3 4 5 6 7 8
-			○ Step 1: swap (1, 8) ⇒ 8 2 3 4 5 6 7 1
-			○ Step 2: swap (2, 7) ⇒ 8 7 3 4 5 6 2 1
-			○ Step 3: swap (3, 6) ⇒ 8 7 6 4 5 3 2 1
-			○ Step 4: swap (4, 6) ⇒ 8 7 6 5 4 3 2 1
-				■ Stop after n/2 steps
-
-
-
-*/
 
 int main() {
 	int N;
