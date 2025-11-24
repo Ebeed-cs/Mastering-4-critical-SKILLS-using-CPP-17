77031e270460f6a44bd3d270243eb17e865fa011
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:26:11 +0200

    Update Nested Loop.cpp

--- 07-Loops/lecture problems - 1/Nested Loop.cpp
+++ 07-Loops/lecture problems - 1/Nested Loop.cpp
@@ -1,19 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-				Nested Loop
-
-	Write a program that reads integer T for T test cases.
-		● Then read T numbers:
-			○ for each number N print sum of 1 to N
-		● Remember, we can replace the sum with formula N * (N+1) / 2
-			○ Which is more efficient?
-
-
-*/
-
 int main() {
 	int T;
 	cin >> T;
