a613ae179a4e1f9c272bfd0f0575e36fc0c42946
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:23:59 +0200

    Update Special Sum.cpp

--- 07-Loops/Lecture Problems - 2/Special Sum.cpp
+++ 07-Loops/Lecture Problems - 2/Special Sum.cpp
@@ -1,24 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-				Special Sum
-	● Read T for number of test cases. For each test case read integer N: number
-		of numbers. Then read N numbers a, b, c, ….. and compute the sum of:
-		○ (a, b*b, c*c*c, d*d*d*d, e*e*e*e*e……)
-		○ That is the k-th number is repeated k times
-
-	● Input:
-		○ 2
-		○ 3   5 7 2
-		○ 4  1 2 3 4
-	● Output
-		○ 62                                         [as (5 + 7*7 + 2*2*2)  = 62]
-		○ 288                                       [as (1+2*2+3*3*3+4*4*4*4) = 288]
-
-
-*/
 
 int main() {
 	int T;
