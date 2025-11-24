19d39f45f4fbabdde4a404b739d1151e7fab5a59
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:21:35 +0200

    Update Line Of Characters.cpp

--- 07-Loops/Easy Level/Line Of Characters.cpp
+++ 07-Loops/Easy Level/Line Of Characters.cpp
@@ -1,16 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-
-		Line Of Characters
-	● Input: Read an integer N followed by a single character
-	● Output: Print the character N times as below
-	● Input ⇒ Output
-		○ 5 Y ⇒ YYYYY
-		○ 3 # ⇒ ###
-
-*/
 
 int main() {
 	int x;
