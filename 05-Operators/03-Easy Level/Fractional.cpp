c98f75b86d9f99ac3ca412fed9aeb515774ef5c8
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:11:49 +0200

    Update Fractional.cpp

--- 05-Operators/03-Easy Level/Fractional.cpp
+++ 05-Operators/03-Easy Level/Fractional.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-	 Fractional Part
- ● Write a program that reads 2 numbers a, b and divides them (a/b), but prints
-	only the fraction part
- ● Input: 201 25
- ● Output: 0.04
- ○ Notice: 201 / 25 = 8.04
- ○ We only want the fraction part: 0.04
-
-
-*/
 int main() {
 	double a, b;
 	cin >> a >> b;
