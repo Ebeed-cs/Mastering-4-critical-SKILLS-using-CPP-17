5a1d8a26eccd1a1062775ed2eb81e62d62cad5e8
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:40:57 +0200

    Update Find first and 2nd Maximum values - 1.cpp

--- 08-1D Arrays/lecture Problems - 1/Find first and 2nd Maximum values - 1.cpp
+++ 08-1D Arrays/lecture Problems - 1/Find first and 2nd Maximum values - 1.cpp
@@ -1,16 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*				Approcah 1
-			 Find first and 2nd Maximum values
-
-	● Read an Integer N ( < 200), then read N (distinct) integers. Find the maximum and 2nd maximum values
-
-	● Input: 5  10 20 3 30 7 ⇒ Output 30 20
-		○ 30 is the maximum in the array
-		○ If we removed it, the next maximum is 20
-
-*/
 
 int main() {
 	int N, max_num = INT_MIN;
