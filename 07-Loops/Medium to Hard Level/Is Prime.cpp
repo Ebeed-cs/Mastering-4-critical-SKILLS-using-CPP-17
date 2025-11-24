1bc8b49ab98faf1c004db3469b5f2b1e842d44a8
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:25:48 +0200

    Update Is Prime.cpp

--- 07-Loops/Medium to Hard Level/Is Prime.cpp
+++ 07-Loops/Medium to Hard Level/Is Prime.cpp
@@ -1,22 +1,6 @@
 #include<iostream>
 #include <cmath>
 using namespace std;
-/*
-							 Is Prime?
- 
-		● Read an integer N (< 500) and print YES if it is prime, otherwise NO
- 			○ A prime number is greater than 1 AND cannot be formed by multiplying two smaller numbers. 
-				■ In other words, number%whatever != 0
-				■ The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23, and 29.
-
-		● Input ⇒ Output 
-			○ 13 ⇒ YES           (only 1 * 13)
-			○ 12 ⇒ NO             (E.g. 12 = 2 *6, so 12 can be divided by 2 or 6)
-
-
-
-
-*/
 
 int main() {
 	int N;
