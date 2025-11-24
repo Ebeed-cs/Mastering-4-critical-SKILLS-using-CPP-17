8a51413700e6d5d7527bf21b3857cdd3490841a1
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:52:27 +0200

    Update Max of 6 numbers.cpp

--- 11-Functions/Functions Homework/Max of 6 numbers.cpp
+++ 11-Functions/Functions Homework/Max of 6 numbers.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
 using namespace std;
-/*
-             Max of 6 numbers
-
-    ● Write a function that reads 6 numbers and compute their maximum. Create the following functions
-        ○ max(int a, int b, int c)
-        ○ max(int a, int b, int c, int d)
-        ○ max(int a, int b, int c, int d, int e)
-        ○ max(int a, int b, int c, int d, int e, int f)
-    ● How can
-        ○ max(int a, int b, int c, int d) utilize max(int a, int b, int c) ? and so on
-*/
 
 int max(int a, int b) {
     if (a >= b)
