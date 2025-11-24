8849a26c53a67af38ca25aef0e09ea6dd9ce4d15
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:57:39 +0200

    Update Power function.cpp

--- 12-Recursive Functions/Easy To Medium Challenges/Power function.cpp
+++ 12-Recursive Functions/Easy To Medium Challenges/Power function.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-        Power function
 
-    ● int my_pow(int value, int p = 2)
-    ● Return value * value ….. * value p times
-    ● E.g. my_pow(7, 3) = 7 * 7 * 7 = 343
-    ● Note: if p = 0, answer is 1
-
-*/
 
 int my_pow(int value, int p = 2) {
     if (p == 0) return 1;
