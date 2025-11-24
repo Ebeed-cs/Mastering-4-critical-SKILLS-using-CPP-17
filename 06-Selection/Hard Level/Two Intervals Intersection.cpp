dbdd1847c51c3e6a1da58f70dee5058ceb08ed19
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:16:26 +0200

    Update Two Intervals Intersection.cpp

--- 06-Selection/Hard Level/Two Intervals Intersection.cpp
+++ 06-Selection/Hard Level/Two Intervals Intersection.cpp
@@ -1,22 +1,6 @@
 #include<iostream>
 using namespace std;
 
-/*
-				[Explanation Video]
-		   Two Intervals Intersection
- ● Read 4 numbers representing 2 intervals and print their intersection interval. If they don’t intersect, print -1
- 
-● Inputs
- 	○ 1 6     3 8    ⇒ 3  6
- 		■ Interval [1 6] and [3 8] only intersects at [3, 6]
-		■ Why: interval [1, 6] has numbers: {1, 2, 3, 4, 5, 6}
-		■ And: interval [3, 8] has numbers: {3, 4, 5, 6, 7, 8}
-		■ So the intersection is {3, 4, 5, 6} = [3, 6]
-	 ○ 1 15  20 30 ⇒ -1
-
-
-*/
-
 
 int main() {
 	int start1, end1 , start2 , end2;
