72022f779bffc0c4f2e0178201e7d921438bb7e1
Ahmed Ebeed <aebeedcs@gmail.com>
2025-11-24 09:43:40 +0200

    Update Is Substring.cpp

--- 09-Char Arrays/Easy to Medium Challenges/Is Substring.cpp
+++ 09-Char Arrays/Easy to Medium Challenges/Is Substring.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-                        Is Substring?
-
-    ● Read 2 strings input and str. Print YES if the given str is a substring for the
-        string. Otherwise, print NO
-
-    ● Input ⇒ Outputs
-        ○ ABCDEFG ABCD ⇒ YES
-        ○ ABCDEFG ABCDEF ⇒ YES
-        ○ ABCDEFG EFG ⇒ YES
-        ○ ABCDEFG DEFG ⇒ YES
-        ○ ABCDEFG BCD ⇒ YES
-        ○ ABCDEFG DEF ⇒ YES
-        ○ ABCDEFG ACEG ⇒ NO
-        ○ ABCDEFG DG ⇒ NO
-        ○ ABCDEFG GD ⇒ NO
-        ○ ABCDEFG ABCDEFG ⇒ YES
-
-*/
 
 int main() {
     string input, str;
